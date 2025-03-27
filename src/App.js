05701ea733e82854773b9da15449cef131abf5f5
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-03-27 15:54:50 -0300

    Projeto iniciado
    - Código básico copiado
    - gitignore atualizado para o padrão vite

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ListaTarefas from './components/ListaTarefas';
+
+import './style.css';
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <h1>Gerenciador de Tarefas</h1>
+            <ListaTarefas />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/ListaTarefas.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+function ListaTarefas() {
+    const [tarefas, setTarefas] = useState([]);
+    const [novaTarefa, setNovaTarefa] = useState('');
+
+    const adicionarTarefa = () => {
+        if (novaTarefa.trim() !== '') {
+            setTarefas([...tarefas, novaTarefa]);
+            setNovaTarefa('');
+        }
+    };
+
+    const removerTarefa = (index) => {
+        setTarefas(tarefas.filete((_, i) => i !== index));
+    };
+
+    return (
+        <div>
+            <h2>Lista de Tarefas</h2>
+            <input 
+                type="text"
+                value={novaTarefa}
+                onChange={(e) => setNovaTarefa(e.target.value)}
+                placeholder='Digite uma nova tarefa'
+            />
+            <button onClick={adicionarTarefa}>Adicionar</button>
+            <ul>
+                {tarefas.map((tarefa, index) => (
+                    <li key={index}>
+                        {tarefa} <button onClick={() =>
+                            removerTarefa(index)}>Remover</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ListaTarefas;
